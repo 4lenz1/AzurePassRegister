d743398f9e5603e7ec83c8673b1296bfef638448
4lenz1 <pingfallenzone@gmail.com>
2015-11-25 12:49:40 +0800

    layout
    op

--- Form1.cs
+++ Form1.cs
@@ -28,33 +28,46 @@ namespace WindowsFormsApplication1
 
         private void browser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
         {
+            string currentUrl = browser.Url.ToString();
+            string[] url = currentUrl.Split('/');
+
+
+            for(int index = 0; index < url.Length; index++)
+                Console.WriteLine("url["+index+"] "+ url[index]);
+
+            // Azure Pass Promo Page
             if (browser.Url.Equals("https://www.microsoftazurepass.com/"))
+           
+
+            //if (url[2].Equals("www.microsoftazurepass.com"))
             {
                 Console.WriteLine("Filling country code and promo code " + browser.Url);
                 string[] attribute = new string[] { "ddlCountry", "tbPromo" };
 
+               //  fillElement; 
                 for (int index = 0; index < attribute.Length; index++)
                 {
                     HtmlElement fillElement = browser.Document.GetElementById(attribute[index]);
-                    fillElement.Focus();
 
-                    switch (index)
-                    {
-                        case 0:
-                            fillElement.SetAttribute("value", "830974e9-147b-e011-8167-001f29c6fb82");
-                            break;
-                        case 1:
-                            fillElement.InnerText = "MCRV7QGJ9EMYXH";
-                            break;
+                        fillElement.Focus();
+
+                        switch (index)
+                        {
+                            case 0:
+                                //set Country value asa Taiwan  
+                                fillElement.SetAttribute("value", "830974e9-147b-e011-8167-001f29c6fb82");
+                                break;
+                            case 1:
+                                //filling promo code here 
+                                fillElement.InnerText = "MCRV7QGJ9EMYXH";
+                                break;
+                        }
+
+                        fillElement.RemoveFocus();
                     }
-
-                    fillElement.RemoveFocus();
-
-                }
+                
 
                 // click submit 
-
-
 
                 HtmlElementCollection input = this.browser.Document.GetElementsByTagName("input");
                 for (int ii = 0; ii < input.Count; ii++)
@@ -76,13 +89,14 @@ namespace WindowsFormsApplication1
                 Console.WriteLine("Account is exsit " + browser.Url );
             }
 
-            else if (browser.Url.Equals("https://login.microsoftonline.com/72f988bf-86f1-41af-91ab-2d7cd011db47/oauth2/authorize?client_id=ca21d81b-8bfb-4c06-99d3-d493cb2d82c6&redirect_uri=https:%2f%2fwww.microsoftazurepass.com%2fRegistration&response_mode=form_post&response_type=code+id_token&scope=openid+profile&state=OpenIdConnect.AuthenticationProperties%3dKQj5ex_oq6feDBK_OjWr6auxfvvI7HMaeCADURzLN6h5mxRl1QCIDRBSxzSrN_i6aoLQKhjONslnvxkO0cAmXYAk4X4q0XZ_FuypZ-lhwyryt1LUF35JoKXZ4wKCijy1FIDYCmEWEG9zqqvUq95gwPUt7fEXIjo4oMhwDxK1TqnW716xm8rV-7Vh_rA-e2JHnuimkzQaeHgDxs29dT1A5mqlNqA&nonce=635827735502309268.NTE5MWU2NmUtYzk2Zi00YjE1LTg5NWUtYmQ2Yjc3MzUwOGU1MzA2ZjBjZWQtNDNiMi00NzQ1LTljMTctMzJmMjdhN2YwNjlm"))
+
+
+
+            else if (url[2].Equals("login.microsoftonline.com"))
             {
                 Console.WriteLine("Login Plz  " + browser.Url);
 
                 string[] attribute = new string[] { "cred_userid_inputtext","cred_password_inputtext"};
-
-
 
                 // fill the element to login 
                 for (int index = 0; index < attribute.Length; index++)
@@ -114,6 +128,38 @@ namespace WindowsFormsApplication1
                     }
                 }
             }
+
+
+
+            // auth login 
+            else if (url[2].Equals("login.live.com"))
+            {
+                Console.WriteLine("Lfillig password   " + browser.Url);
+
+
+                // fill the element to login 
+
+                HtmlElement fillElement = browser.Document.GetElementById("i0118");
+                fillElement.Focus();
+                fillElement.InnerText = "MS20151110";
+
+                fillElement.RemoveFocus();
+
+                // click submit 
+                HtmlElementCollection input = this.browser.Document.GetElementsByTagName("input");
+                for (int ii = 0; ii < input.Count; ii++)
+                {
+                    if (input[ii].GetAttribute("type").ToLower().Equals("submit"))
+                    {
+                        input[ii].InvokeMember("click");
+                    }
+                }
+            }
+
+
+
+
+
             else
             {
                 Console.WriteLine("Unknow Status " + browser.Url);
